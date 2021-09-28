76acc118ee1e4215a17635e5ca1b6a00ed14cc0b
Enjyyy <bluestacksdemo9@gmail.com>
2021-09-28 23:04:18 +0200

    modification#1

---
+++ lib/screens/sign_in.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/custom_button.dart';
+import 'package:food_app/custom_text_field.dart';
+
+class SignInScreen extends StatelessWidget {
+  static const String id = 'SignInScreen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            'Welcome Back!',
+            style: TextStyle(
+                color: Color(0xffd8434d),
+                fontSize: 50,
+                fontWeight: FontWeight.w700),
+          ),
+          Image.asset('assets/salad.png'),
+          CustomTextField(
+            icon: Icons.email,
+            labelText: 'Email',
+            function: (value) {
+              //TODO: implement firebase auth
+            },
+          ),
+          CustomTextField(
+            icon: Icons.password,
+            labelText: 'Password',
+            obscure: true,
+            function: (value) {
+              //TODO: implement firebase auth
+            },
+          ),
+          CustomButton(
+              text: 'Sign In',
+              function: () {
+                //TODO: Navigate to home screen
+              })
+        ],
+      )),
+    );
+  }
+}
