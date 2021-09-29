3626d5f87c78c8f7cd42e8d823f105007aa7dda4
mohamed <mohamedyossif558@gmail.com>
2021-09-30 01:42:25 +0200

    firebase and Api

---
+++ lib/services/auth_firebase.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthFirebaseMethods {
+   FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future signUpWithEmailAndPassword(context, email, password) async {
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future signInWithEmailAndPassword(context, email, password) async {
+    try {
+       await _firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password);
+      
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+      print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+      print( 'Wrong password provided for that user.');
+      }
+    }
+  }
+
+  Future signOut() async {
+    try {
+      return await _firebaseAuth.signOut();
+    } catch (e) {
+      print(e);
+    }
+  }
+}
